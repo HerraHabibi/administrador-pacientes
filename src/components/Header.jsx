7d1a8680bc2af64ef5cb48c5e02da112992ac8ac
HerraHabibi <javierre13@hotmail.com>
2023-11-12 00:05:04 +0100

    Clases en el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
+      Seguimiento Pacientes {''}
+      <span className='text-sky-500'>Veterinaria</span>
+    </h1>
   );
 }
 
