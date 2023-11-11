cdd3f0e198371601a3039fbdf6d5d612b9d8d526
HerraHabibi <javierre13@hotmail.com>
2023-11-11 23:57:01 +0100

    Primeras clases con Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import Formulario from './components/Formulario';
 import Header from './components/Header';
 import ListadoPacientes from './components/ListadoPacientes';
 
-
 function App() {
   return (
-    <>
+    <div className='container mx-auto mt-20'>
       <Header />
       <Formulario />
       <ListadoPacientes />
-    </>
+    </div>
   );
 }
 
