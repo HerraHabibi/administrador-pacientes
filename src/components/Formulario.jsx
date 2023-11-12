4cbce161cf88bf63b69350838b4f780aaa1a823a
HerraHabibi <javierre13@hotmail.com>
2023-11-12 01:04:31 +0100

    Diseño del componente Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,16 @@
 function Formulario() {
   return (
-    <>
-      <h2>Formulario</h2>
-    </>
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+      <p className='text-lg mt-5 text-center'>
+        Añade Pacientes y {''}
+        <span className='text-sky-500 font-bold'>Adminístralos</span>
+      </p>
+
+      <form>
+        
+      </form>
+    </div>
   );
 }
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,8 @@
 function ListadoPacientes() {
   return (
-    <>
+    <div className='md:w-1/2 lg:w-3/5'>
       <h2>Listado de pacientes</h2>
-    </>
+    </div>
   );
 }
 
